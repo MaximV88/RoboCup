d2bb96aef68d4594c2901e9fdf5f1c36f4ceaf48
Maxim <maximv88@gmail.com>
2015-01-14 19:04:16 +0200

    Initial Commit

---
+++ Robocup/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Robocup
+//
+//  Created by Maxim Vainshtein on 1/14/15.
+//  Copyright (c) 2015 Maxim Vainshtein. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
